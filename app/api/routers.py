c81d8507886d3b2d7ba7197d2c413e71522127c2
uliazaharova <sorulai@yandex.ru>
2024-05-22 01:06:48 +0300

    fix routers

--- app/api/routers.py
+++ app/api/routers.py
@@ -12,7 +12,15 @@ router = APIRouter()
 @router.post("/write_data")
 async def write_data(data: DataModel,
                      redis: Annotated[Redis, Depends(init_redis_pool)]):
-    if not await redis.exists(data.phone):
+    """
+    Ендпоинт для записи данных в хранилище Redis
+    :param data: Данные для записи - номер телефона(валидация происходит на стороне pydantic
+    по регулярному выражению [78]\d{10}$) и адрес
+    :param redis: Экземпляр клиента Redis
+    :return: Если запись прошла успешно, возвращается 201 статус,
+    если не удалось записать данные, то возвращается статус 500
+    """
+    try:
         await redis.set(name=data.phone, value=data.address)
         return JSONResponse(
             status_code=status.HTTP_201_CREATED,
@@ -20,28 +28,22 @@ async def write_data(data: DataModel,
                 'phone': data.phone,
                 'address': data.address
             })
-    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                        detail="This is phone was created yet")
-
-
-@router.put("/write_data")
-async def write_data(data: DataModel,
-                     redis: Annotated[Redis, Depends(init_redis_pool)]):
-    if await redis.exists(data.phone):
-        await redis.set(name=data.phone, value=data.address)
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content={
-                'phone': data.phone,
-                'address': data.address
-            })
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                        detail="Phone number not found")
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="Something went wrong")
 
 
 @router.get("/check_data", response_model=ResponseCheckData)
 async def check_data(phone: Annotated[str, Query(..., pattern=r"^8\d{10}$")],
                      redis: Annotated[Redis, Depends(init_redis_pool)]):
+    """
+    Эндпоинт для получения адреса по номеру телефона.
+    :param phone: Номер телефона - валидация происходит на стороне pydantic
+    по регулярному выражению [78]\d{10}$
+    :param redis: Экземпляр клиента Redis
+    :return: Если номера телефона не найден, возвращается ошибка 404,
+    если запрос прошел успешно, то возвращается адрес
+    """
     address = await redis.get(phone)
     if address is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
